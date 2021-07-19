0a2cb796b945c2cb2cf563350f48339c7c3d658f
LieneTamuza <76061709+LieneTamuza@users.noreply.github.com>
2021-07-19 17:07:36 +0300

    Add header

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Footer from "./Components/Footer";
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
     return (
         <div>
             <Router>
+                <Header />
                 <Footer />
             </Router>
         </div>
---
+++ front-end-react/src/Components/Header.js
@@ -0,0 +1,40 @@
+import { NavLink } from 'react-router-dom';
+import logo from '../Assets/Images/logo.png';
+
+function Header() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <NavLink to="/" className="navbar-brand">
+                        <img src={logo} height="30" alt="Logo" />
+                    </NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink exact to="/" className="nav-link">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/news" className="nav-link">News</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/chat" className="nav-link">Chat</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/register" className="nav-link">Register</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/about" className="nav-link">About</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
