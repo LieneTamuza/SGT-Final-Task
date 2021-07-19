361a1e48d39564948ef2b127ba5e6d72b1ea8429
LieneTamuza <76061709+LieneTamuza@users.noreply.github.com>
2021-07-19 17:15:05 +0300

    Add 404 page

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -1,12 +1,20 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Page404 from './Views/Page404';
 
 function App() {
     return (
         <div>
             <Router>
                 <Header />
+
+                <Switch>
+                    <Route path=''>
+                        <Page404 />
+                    </Route>
+                </Switch>
+
                 <Footer />
             </Router>
         </div>
