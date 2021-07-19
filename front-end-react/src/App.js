64d36a36a35f9572d30f784d1aee0fd33dbbc110
LieneTamuza <76061709+LieneTamuza@users.noreply.github.com>
2021-07-19 16:48:06 +0300

    Add footer

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -1,9 +1,12 @@
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Footer from "./Components/Footer";
 
 function App() {
     return (
         <div>
-
+            <Router>
+                <Footer />
+            </Router>
         </div>
     );
 }
---
+++ front-end-react/src/Components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <div className='container-fluid mt-5 py-2 px-3 bg-light fixed-bottom'>
+            <div className="row">
+                <span className="text-secondary">&copy; 2021 Easter Prize</span>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
