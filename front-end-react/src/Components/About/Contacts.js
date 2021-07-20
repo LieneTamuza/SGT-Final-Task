8c7230c73f4bf41125e666f4088945c3c7a68e5c
LieneTamuza <76061709+LieneTamuza@users.noreply.github.com>
2021-07-20 13:34:17 +0300

    Add Google Maps

--- front-end-react/src/Components/About/Contacts.js
+++ front-end-react/src/Components/About/Contacts.js
@@ -1,7 +1,44 @@
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
 function Contacts() {
+    const mapStyles = {
+        height: "50vh",
+        width: "100%",
+        borderRadius: "5px"
+    };
+
+    const defaultCenter = {
+        lat: 56.96193048089572, lng: 24.23678462743227
+    };
+
+    const locations = [
+        {
+            name: "Event center",
+            location: {
+                lat: 56.96193048089572,
+                lng: 24.23678462743227
+            },
+        },
+    ];
+
     return (
         <div className="container mb-5">
             <h4>Contacts</h4>
+            <LoadScript
+                googleMapsApiKey=''>
+                <GoogleMap
+                    mapContainerStyle={mapStyles}
+                    zoom={13}
+                    center={defaultCenter}>
+                    {
+                        locations.map(item => {
+                        return (
+                            <Marker key={item.name} position={item.location} />
+                        );
+                    })
+                    }
+                </GoogleMap>
+            </LoadScript>
         </div>
     );
 }
