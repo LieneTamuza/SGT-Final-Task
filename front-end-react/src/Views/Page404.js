439e22bbb34963d77530f4f5d0073a285228fafa
LieneTamuza <76061709+LieneTamuza@users.noreply.github.com>
2021-07-21 16:40:07 +0300

    Update Page404.js

--- front-end-react/src/Views/Page404.js
+++ front-end-react/src/Views/Page404.js
@@ -2,18 +2,15 @@ import { NavLink } from 'react-router-dom';
 
 function Page404() {
     return (
-        <div className="container px-4 py-5 my-5 text-center error-template">
-            <h1 className="display-5 fw-bold">Oops!</h1>
-            <h2 className="display-7 fw-bold">404 Not Found</h2>
+        <div className="container px-4 py-5 my-5 text-center">
+            <h1 className="display-5 fw-bold">404</h1>
+            <h2 className="display-7 fw-bold">LOOKS LIKE YOU'RE LOST!</h2>
             <div className="col-lg-6 mx-auto error-details">
-                <p className="lead mb-4">Sorry, an error has occured. Requested page not found!</p>
+                <p className="mb-4">The page that you are looking for is not available</p>
                 <div className="d-grid gap-2 d-sm-flex justify-content-sm-center error-actions">
                     <NavLink className="nav-link" exact to="/">
-                        <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Take Me Home</button>
+                        <button type="button" className="btn btn-primary btn-md px-4">Home</button>
                     </NavLink>
-                    {/* <NavLink className="nav-link" to="/">
-                    <button type="button" className="btn btn-outline-secondary btn-lg px-4">Contact Support</button>
-                </NavLink> */}
                 </div>
             </div>
         </div>
