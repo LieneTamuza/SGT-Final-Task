8aaf36d76c93071e4340d68065c002878e0d13b0
LieneTamuza <76061709+LieneTamuza@users.noreply.github.com>
2021-07-22 10:57:59 +0300

    Format links, titles and buttons

--- front-end-react/src/Components/Header.js
+++ front-end-react/src/Components/Header.js
@@ -3,11 +3,12 @@ import { Squash as Hamburger } from 'hamburger-react';
 import logo from '../Assets/Images/logo.png';
 
 function Header() {
+
     return (
         <div className="container">
             <nav className="navbar navbar-expand-lg navbar-light">
                 <div className="container-fluid">
-                    <NavLink to="/" className="navbar-brand">
+                    <NavLink to="/" className="navbar-brand px-1">
                         <img src={logo} height="30" alt="Logo" />
                     </NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,22 +16,22 @@ function Header() {
                             <span className="navbar-toggler-icon"></span>
                         </Hamburger>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <NavLink exact to="/" className="nav-link">Home</NavLink>
+                                <NavLink exact to="/" className="nav-link fw-bolder px-3">Home</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/news" className="nav-link">News</NavLink>
+                                <NavLink to="/news" className="nav-link fw-bolder px-3">News</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/forum" className="nav-link">Forum</NavLink>
+                                <NavLink to="/forum" className="nav-link fw-bolder px-3">Forum</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/register" className="nav-link">Register</NavLink>
+                                <NavLink to="/register" className="nav-link fw-bolder px-3">Register</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/about" className="nav-link">About</NavLink>
+                                <NavLink to="/about" className="nav-link fw-bolder px-3">About</NavLink>
                             </li>
                         </ul>
                     </div>
