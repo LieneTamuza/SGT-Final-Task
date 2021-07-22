f882bf136008a961c717a6bb1124b886f909caed
LieneTamuza <76061709+LieneTamuza@users.noreply.github.com>
2021-07-22 11:10:59 +0300

    Add image to the 404 page

--- front-end-react/src/Views/Page404.js
+++ front-end-react/src/Views/Page404.js
@@ -1,13 +1,16 @@
 import { NavLink } from 'react-router-dom';
+import map from '../Assets/Images/page_404.png';
 
 function Page404() {
+    
     return (
-        <div className="container px-4 py-5 my-5 text-center">
-            <h1 className="display-5 fw-bold">404</h1>
-            <h2 className="display-7 fw-bold">LOOKS LIKE YOU'RE LOST!</h2>
-            <div className="col-lg-6 mx-auto error-details">
-                <p className="mb-4">The page that you are looking for is not available</p>
-                <div className="d-grid gap-2 d-sm-flex justify-content-sm-center error-actions">
+        <div className="container p-4 text-center">
+            <h1 className="display-6 fw-bold">404</h1>
+            <h3 className="fw-bold">Looks like you're lost!</h3>
+            <div className="col-lg-6 mx-auto">
+                <p className="mb-4">But since you're here, take a look at the previous map!</p>
+                <img src={map} height="555px" alt="Map" />
+                <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
                     <NavLink className="nav-link" exact to="/">
                         <button type="button" className="btn btn-primary btn-md px-4">Home</button>
                     </NavLink>
